6b640c67e45fc0e67ea355c8d3733371ae13f520
alexpurhalo <alexpurhalo@gmail.com>
2016-08-28 17:18:06 +0300

    max-avg-min-values-definition

--- src/components/analysis/simple_analysis.js
+++ src/components/analysis/simple_analysis.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 export default class SimpleAnalysis extends Component {
+	// Basic dat definition and input handling --- Part I
 	constructor() {
 		super();
 
@@ -16,6 +17,24 @@ export default class SimpleAnalysis extends Component {
 			: this.setState({ dataCond: 'Waiting for correct data' })
 	}
 
+	// Simple analysis (avg, max, min) ---- Part II
+	maxItemDefinition() {
+		return Math.max(...this.state.value)
+	}
+
+	minItemDefinition() {
+		return Math.min(...this.state.value)
+	}
+
+	averageItemDefinition() {
+		let total = 0, arr = this.state.value;
+		for(let i = 0; i < arr.length; i++) {
+			total += parseInt(arr[i]);
+		}
+
+		return total / arr.length;
+	}
+
 	render() {
 		return (
 			<div>
@@ -26,6 +45,18 @@ export default class SimpleAnalysis extends Component {
 					className="form-control"
 					onChange={this.handleAnalysis} />
 				<h5>{this.state.dataCond}</h5>
+				{this.state.value.length > 2 ? (
+					<ul >
+						<li>Max: {this.maxItemDefinition()}</li>
+						<li>Min: {this.minItemDefinition()}</li>
+						<li>Avg: {this.averageItemDefinition()}</li>
+					</ul>
+				) : <div>On this place will appear your analysis</div>}
+				<br/><br/>
+				<button
+					className="btn btn-primary"
+					onClick={this.props.handleCorrelation}>
+					Go to correlation</button>
 			</div>
 		);
 	}
